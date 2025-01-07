2fb3e87065ae387bd03ec739999e772e75e86abd
ananthas-30 <dpananth3024@gmail.com>
2025-01-07 19:53:16 +0530

    Update About.jsx

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
            <p>I’m a frontend developer passionate about creating sleek, user-friendly websites and applications. With expertise in HTML, Tailwind CSS, JavaScript, and modern frameworks like React, I bring designs to life and ensure a seamless user experience.</p>
            <div className='experience'><span>Experience:</span>
             <p>Frontend developer at Reveille Technologies.<br/>
-             Jun, 2024.</p>
+             Jan , 2024.</p>
            </div>
            <Button buttonLInk={Resume} buttonText={'Resume'} />
         </div>
